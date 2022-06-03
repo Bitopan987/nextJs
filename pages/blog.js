839e9c4bcdb8a6a8c2875d845384d4a6b2a5e850
bitopan987 <bitopanxon1@gmail.com>
2022-06-03 17:33:51 +0530

    refactor

--- pages/blog.js
+++ pages/blog.js
@@ -2,6 +2,7 @@ import styles from '../styles/blog.module.css';
 import Image from 'next/image';
 import Navbar from '../components/Navbar';
 import Form from '../components/Form';
+import { POSTS } from '../utils/constants';
 
 export default function Blog() {
   return (
@@ -10,23 +11,12 @@ export default function Blog() {
       <h1 className={styles.title}>Welcome to Blog Page</h1>
       <p className={`${styles.text} `}>Blogs by Bitopan</p>
       <article className={styles.flex}>
-        <div>
-          <Image src="/nature3.jpg" width="300" height="200"></Image>
-          <p>This is column One</p>
-        </div>
-        <div>
-          <Image src="/nature3.jpg" width="300" height="200"></Image>
-          <p>This is column One</p>
-        </div>
-
-        <div>
-          <Image src="/nature3.jpg" width="300" height="200"></Image>
-          <p>This is column One</p>
-        </div>
-        <div>
-          <Image src="/nature3.jpg" width="300" height="200"></Image>
-          <p>This is column One</p>
-        </div>
+        {POSTS.map(({ image, title }) => (
+          <div>
+            <Image src={image} width="300" height="200"></Image>
+            <p>{title}</p>
+          </div>
+        ))}
       </article>
       <Form />
     </div>
---
+++ utils/constants.js
@@ -0,0 +1,22 @@
+export const POSTS = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1654027277479-a481adfdfcd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+    title: 'This is post one',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1652900439414-c0e834dcfa2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE3fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    title: 'This is post two',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1653085628432-df36a4fa60a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    title: 'This is post three',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1653958531645-fef7d3b83fc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    title: 'This is post four',
+  },
+];
